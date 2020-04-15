083ed7336217b9df717ee15eb00ec5b03a271c75
YungSushiRoll <j.gosset1@gmail.com>
2020-04-15 14:37:17 -0500

    Add project files.

---
+++ TicketSystemPt2/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+
+namespace TicketSystemPt2
+{
+    class Program
+    {
+        // create a class level instance of logger (can be used in methods other than Main)
+        private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+        static void Main(string[] args)
+        {
+            string file = "Tickets.csv";
+            logger.Info("Program started");
+
+            TicketFile ticketFile = new TicketFile(file);
+            string choice;
+
+            do
+            {
+                Console.WriteLine("1) View Ticket file Summary.");
+                Console.WriteLine("2) Create Ticket file.");
+                Console.WriteLine("Enter any other key to exit.");
+
+                choice = Console.ReadLine();
+
+                if (choice == "1")
+                {
+                    foreach (Ticket t in ticketFile.Tickets)
+                    {
+                        Console.WriteLine(t.Output());
+                    }
+                }
+                else if (choice == "2")
+                {
+                    Ticket ticket = new Ticket();                    
+
+                    Console.WriteLine("Enter a new ticket? (Y/N)");
+                    string newTicket = Console.ReadLine().ToUpper();
+                    if (newTicket != "Y") { break; }
+
+                    Console.WriteLine("Enter Ticket Summary:");
+                    ticket.summary = Console.ReadLine();
+
+                    Console.WriteLine("Enter Ticket Status:");
+                    ticket.status = Console.ReadLine();
+
+                    Console.WriteLine("Enter Ticket Priority:");
+                    ticket.priority = Console.ReadLine();
+
+                    Console.WriteLine("Enter Ticket Submitter:");
+                    ticket.submitter = Console.ReadLine();
+
+                    Console.WriteLine("Ticket Assigned to:");
+                    ticket.assigned = Console.ReadLine();
+
+                    string tixWatcher;
+                    string theWatchers;
+                    do
+                    {
+                        Console.WriteLine("Is there someone watching this ticket? (Y/N)");
+                        tixWatcher = Console.ReadLine();
+                        if (!tixWatcher.ToUpper().Equals("Y")) { break; }
+                        Console.WriteLine("Who is watching the ticket?");
+                        theWatchers = Console.ReadLine();
+                        ticket.watchers.Add(theWatchers);
+                    } while (tixWatcher.ToUpper().Equals("Y"));
+                    ticketFile.AddTicket(ticket);
+                }
+
+
+            } while (choice == "1" || choice == "2");
+
+            logger.Info("Program ended");
+
+        }
+    }
+}
---
+++ TicketSystemPt2/Ticket.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicketSystemPt2
+{
+    public class Ticket
+    {
+        public int ticketId { get; set; }
+        public string summary { get; set; }
+        public string status { get; set; }
+        public string priority { get; set; }
+        public string submitter{ get; set; }
+        public string assigned { get; set; }
+        public List<string> watchers { get; set; }
+
+        public Ticket()
+        {
+            watchers = new List<string>();
+        }
+
+        public string Output()
+        {
+            return ticketId + " " + summary + " " + status + " " + priority + " " + submitter + " " + assigned + " " + string.Join("|",watchers);
+        }
+
+    }
+}
